5881ac3ed59b040df4ef356a06f4af823455aad2
hicks27 <hicksp2597@student.faytechcc.edu>
2017-06-18 15:41:09 -0700

    Add files via upload
    M3HW1 Completed

---
+++ M3HW1_AgeClassifier_PatriciaHicks.py
@@ -0,0 +1,5 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 17:54:52) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/Patricia Hicks/AppData/Local/Programs/Python/Python36-32/Age Classifier.py 
+Please enter your age:
